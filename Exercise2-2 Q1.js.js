f9a50156c2b39b02779a42efb02322b276d9ab78
Thomas, ku <112286158+p1704474@users.noreply.github.com>
2022-10-11 01:05:59 +0900

    Update Exercise2-2 Q1.js.js

--- Exercise2-2 Q1.js.js
+++ Exercise2-2 Q1.js.js
@@ -1,7 +1,7 @@
 // 2-2 Q1. Write a program to calculate the sum of even 
 // positive numbers up to 30.
 // The answer should be 240.
-//P1902091 Simon
+//P1704474_Thomas
 
 let i = 1; 
 let sum = 0;
